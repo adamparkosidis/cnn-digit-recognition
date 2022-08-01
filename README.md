b0d8a089fd43e6ce049d69cf6c1c985b5605ead1
adamaparkosidis <adamaparkosidis@gmail.com>
2022-08-01 14:25:48 +0300

    update readme and digit recognition

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# A convolutional network in [https://pytorch.org/tutorials/beginner/pytorch_with_examples.html](PyTorch) to recognize handwritten digits as given in the MNIST dataset
+# A convolutional network in PyTorch to recognize handwritten digits as given in the MNIST dataset
 
+[PyTorch Documentation](https://pytorch.org/tutorials/beginner/pytorch_with_examples.html)
 [Description of the dataset we will be working with](https://en.wikipedia.org/wiki/MNIST_database)
 
 
@@ -8,3 +9,13 @@
 ## Using GPU
 
 One advantage of using pytorch as a framework is that it allows us to execute our code on the GPU. This can often greatly reduce the runtime needed to train neural networks. Below is a short description of how to do this.
+
+
+### CNN
+
+Below you can see a diagram for a convolutional network. THe diagram is translated into a PyTorch model by filling in the `Net` class below.
+
+After each max pooling step and after the first dense (linear) layer apply the relu activation function.
+We use the modules `nn.Conv2d`, `nn.Linear` and the functions `F.max_pool2d` and `F.relu`.
+
+*Hint: Carefully inspect the shapes of the intermediate layers and add padding to the convolutions where necessary.*
--- digit-recognition.py
+++ digit-recognition.py
@@ -40,3 +40,37 @@ test_loader = torch.utils.data.DataLoader(
                   (0.1307,), (0.3081,)),
           ])),
     batch_size=batch_size_test, shuffle=True)
+
+# The transform statement is used to transform the data. More specifically, we see that transforms the data in to tensor type
+# (`torchvision.transforms.ToTensor()`) and also normilizes the data (`torchvision.transforms.Normalize(0.1307,), (0.3081,)`),
+# where the parameters are the mean and standard deviation of the MNIST dataset. To tackle the model learning difficulty, we
+# normalize training data. We ensure that the value ranges of the various features are comparable (feature scaling) so that gradient
+# descents can converge faster. Finally, `shuffle=True`, because it aids in the training's rapid convergence, it eliminates any
+# prejudice during training and inhibits the model from learning the training order.
+
+examples = list(test_loader)
+example_data, example_targets = examples[0]
+print(example_data.shape)
+print(example_targets.shape)
+
+# The `example_data` contains 1000 object (`len(example_data)`)=1000, where each element is an image of hand-written digits between
+# 0 and 9. Each of these objects contains 28 arrays (`len(example_data[0][0])`=28) and each of the 28 arrays contains 28 elements
+#(`len(example_data[0][0][0])`=28). These are the $28 \times 28$ pixels of each image (28 rows $\times$ 28 columns). Each of these
+# 784 elements contains a value between [-0.4242, 2.8215] which indicates the color of the pixel, where $-0.4242$ corresponds to total
+# black while $2.8215$ to total white. The printed shape of the `example_data` is `torch.Size([1000, 1, 28, 28])`, where number 1 just
+# states that the array has 1000 rows and 1 column. This is because `example_data` is a tensor and we need it in this form to able to
+# perform tensor multiplications. The `example_targets` tensor contains 1000 elements, where each elements represent the actual digit
+# that corresponts to each image of the `example_data`. For example, `example_targets[0] = 3` which means that `example_data[0]` containts
+# an image of the hand-written number 3.
+
+
+# Let's visualize some of our test data.
+
+fig = plt.figure(figsize=(20, 10))
+for i in range(40):
+  plt.subplot(5,8,i+1)
+  plt.tight_layout()
+  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+  plt.title("Ground Truth: {}".format(example_targets[i]))
+  plt.xticks([])
+  plt.yticks([])
